21b147a9824f20f17c2735ba7fd513ddadb45c45
nyoungstudios <nathanielyoung95070@gmail.com>
2018-09-22 15:42:19 -0400

    Commit

--- shell.cc
+++ shell.cc
@@ -5,11 +5,50 @@
 int yyparse(void);
 
 void Shell::prompt() {
-  printf("myshell>");
-  fflush(stdout);
+	if (isatty(0)) {
+		printf("myshell>");
+		fflush(stdout);
+	}
 }
 
+void controlC(int sig) {
+	printf("\n");
+	Shell::_currentCommand.clear();
+	Shell::_prompt();
+	
+}
+
+void zombie(int sig) {
+	while (waitpid(-1, NULL, WNOHANG) > );
+}
+
+
 int main() {
+
+	int error;
+
+	struct sigaction sa1;
+	sa1.sa_handler = controlC;
+	sigemptyset(&sa1.sa_mask);
+	sa1.sa_flags = SA_RESTART;
+	error = sigaction(SIGINT, &sa1, NULL);
+	if (error == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	struct sigaction sa2;
+	sa2.sa_handler = zombie;
+	sa2.sa_flags = SA_RESTART;
+	sigemptyset(&sa2.sa_mask);
+	error = sigaction(SIGCHLD, &sa2, NULL);
+	if (error == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+
+
   Shell::prompt();
   yyparse();
 }
