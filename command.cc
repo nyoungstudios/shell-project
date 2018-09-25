2b007b224ede90e48ad06925fcc468de7c172ff0
nyoungstudios <nathanielyoung95070@gmail.com>
2018-09-25 16:13:44 -0400

    Commit

--- command.cc
+++ command.cc
@@ -196,8 +196,8 @@ void Command::execute() {
 				std::string oldpwdString = "OLDPWD=";
 				std::string pwd = getenv("PWD");
 				//pwdString.append(_simpleCommands[0]->_arguments[1]->c_str());
-				setenv("PWD", _simpleCommands[0]->_arguments[1]->c_str());
-				setenv("OLDPWD", pwd.c_str());
+				setenv("PWD", _simpleCommands[0]->_arguments[1]->c_str(), 1);
+				setenv("OLDPWD", pwd.c_str(), 1);
 				//putenv(const_cast<char *>(pwdString.c_str()));
 				//oldpwdString.append(pwd);
 				//putenv(const_cast<char *>(oldpwdString.c_str()));
