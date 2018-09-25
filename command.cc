8e6b38994e42c826f3d84f759c31189d4c33d182
nyoungstudios <nathanielyoung95070@gmail.com>
2018-09-25 01:31:35 -0400

    Commit

--- command.cc
+++ command.cc
@@ -166,7 +166,7 @@ void Command::execute() {
 
 
 			if (error < 0) {
-				fprintf(stderr, "cd: can't cd to %s\n", _simpleCommands[0]->_arguments[1]->c_str());
+				fprintf(stderr, "/bin/sh: 1: cd: can't cd to %s\n", _simpleCommands[0]->_arguments[1]->c_str());
 			}
 			clear();
 			Shell::prompt();
