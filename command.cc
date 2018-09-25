869ee65ea3ca1e70c57efde870f423033e79416e
nyoungstudios <nathanielyoung95070@gmail.com>
2018-09-25 15:43:32 -0400

    Commit

--- command.cc
+++ command.cc
@@ -168,6 +168,10 @@ void Command::execute() {
 		if (!strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "cd")) {
 			int error;
 			if (_simpleCommands[0]->_arguments.size() == 1 || !strcmp(_simpleCommands[0]->_arguments[1]->c_str(), "~")) {
+				std::string pwd = "PWD=";
+				std::string oldpwd = getenv("PWD");
+				pwd.append(oldpwd);
+				putenv(pwd);
 				error = chdir(getenv("HOME"));
 			} else if(_simpleCommands[0]->_arguments.size()== 1 || !strcmp(_simpleCommands[0]->_arguments[1]->c_str(), "-")) {
 				error = chdir(getenv("OLDPWD"));
