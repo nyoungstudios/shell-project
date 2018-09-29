2b8b7dc2bc816d1a966567fb4c97290161c1cb81
nyoungstudios <nathanielyoung95070@gmail.com>
2018-09-28 23:20:09 -0400

    Commit

--- command.cc
+++ command.cc
@@ -372,7 +372,7 @@ void Command::execute() {
 					}
 					exit(0);
 				
-				} else if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "ls")  && _simpleCommands[i]->arguments.size() > 1 && !strcmp(_simpleCommands[i]->_arguments[1]->c_str(), "~")) {
+				} else if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "ls")  && _simpleCommands[i]->_arguments.size() > 1 && !strcmp(_simpleCommands[i]->_arguments[1]->c_str(), "~")) {
 					printf("this is it\n");
 					_simpleCommands[i]->_arguments[1] = new std::string(getenv("HOME"));
 				}
