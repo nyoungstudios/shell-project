ccfd8845fc49a3a2be200270d96f962b584111a0
nyoungstudios <nathanielyoung95070@gmail.com>
2018-09-29 14:33:58 -0400

    Commit

--- command.cc
+++ command.cc
@@ -364,26 +364,24 @@ void Command::execute() {
 				//if ls to home directory
 				if (!strcmp(_simpleCommands[0]->_arguments[1]->c_str(), "~")) {
 					_simpleCommands[0]->_arguments[1] = new std::string(getenv("HOME"));
+				} else if ((const char) *_simpleCommands[0]->_arguments[1]->c_str() == '~') {
+					//if ls to home directory plus path
+					std::string newPath = getenv("HOME");
+				
+					std::string secondPart = (const char *) (_simpleCommands[0]->_arguments[1]->c_str() + 1);
+
+					if ((const char) *(_simpleCommands[0]->_arguments[1]->c_str() + 1) == '/') {
+						newPath += secondPart;
+					} else {
+						newPath += "/";
+						newPath += secondPart;
+					}
+
+					_simpleCommands[0]->_arguments[1] = new std::string(newPath);
+
 				}
 
-			} else if (!strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "ls")  && _simpleCommands[0]->_arguments.size() == 2 && (const char) *_simpleCommands[0]->_arguments[1]->c_str() == '~') {
-				//if ls to home directory plus path
-				std::string newPath = getenv("HOME");
-			
-				std::string secondPart = (const char *) (_simpleCommands[0]->_arguments[1]->c_str() + 1);
-
-				if ((const char) *(_simpleCommands[0]->_arguments[1]->c_str() + 1) == '/') {
-					newPath += secondPart;
-				} else {
-					newPath += "/";
-					newPath += secondPart;
-				}
-
-				_simpleCommands[0]->_arguments[1] = new std::string(newPath);
-			
-
-			}
-		
+			} 		
 			
 			//fork
 			ret = fork();
