5b2f7066adcdcabde45f834d7794f0c82c3ccd35
nyoungstudios <nathanielyoung95070@gmail.com>
2018-09-29 15:39:14 -0400

    Commit

--- command.cc
+++ command.cc
@@ -403,7 +403,47 @@ void Command::execute() {
 					}
 
 				}*/
-		
+				if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "source") {
+					FILE *fp = fopen(_simpleCommands[i]->_arguments[1], "r");
+					char cmdline [1024];
+
+					fgets(cmdline, 1023, fp);
+					fclose(fp);
+
+					int tmpin = dup(0);
+					int tmpout = dup(1);
+
+					int fdpipein[2];
+					int fdpipeout[2];
+
+					pipe(fdpipein);
+					pipe(fdpipeout);
+
+					write(fdpipe[1], cmdline, strlen(cmdline));
+					write(fdpipein[1], "\n", 1);
+
+					close(fdpipein[1]);
+
+					dup2(fdpipein[0], 0);
+					close(fdpipein[0]);
+					dup2(fdpipeout[1], 1);
+					close(fdpipeout[1]);
+
+					int ret2 = fork();
+					if (ret2 == 0) {
+						execvp("/proc/self/exe", NULL);
+						_exit(1);
+					} else if (ret < 0) {
+						perror("fork");
+						exit(1);
+					}
+					
+					dup2(tmpin, 0);
+					dup2(tmpout, 1);
+					close(tmpin);
+					close(tmpout);
+
+				} else {	
 		
 				//convert to char** from vector
 				char** cargument = new char*[_simpleCommands[i]->_arguments.size()];
@@ -416,7 +456,7 @@ void Command::execute() {
 				//execute command
 				execvp(_simpleCommands[i]->_arguments[0]->c_str(), cargument);
 
-
+				}
 			} else if (ret < 0) {
 				perror("fork");
 				return;
