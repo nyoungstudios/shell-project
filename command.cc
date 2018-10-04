060d61afdd7aeec0c5e8d03126dc886343455ff7
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-04 00:40:13 -0400

    Commit

--- command.cc
+++ command.cc
@@ -381,12 +381,12 @@ void Command::execute() {
 					FILE *fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
 					char cmdline [4096];
 
-					while (*fp != EOF) {
-						fgets(cmdline, 4095, fp) 
+					fgets(cmdline, 4095, fp); 
+					fgets(cmdline, 4095, fp); 
+					fgets(cmdline, 4095, fp); 
 		
 				
-						printf("%s\n", cmdline);
-					}
+					printf("%s\n", cmdline);
 					
 					fclose(fp);
 
