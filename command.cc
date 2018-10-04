0b42445a3a09e40a8d4fe60b67ee45b997863951
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-04 01:07:32 -0400

    Commit

--- command.cc
+++ command.cc
@@ -443,23 +443,23 @@ void Command::execute() {
 
 					exit(0);
 
-				}
-		
-				//convert to char** from vector
-				char** cargument = new char*[_simpleCommands[i]->_arguments.size()];
-				unsigned int j;
-				for (j = 0; j < _simpleCommands[i]->_arguments.size(); j++) {
-					cargument[j] = const_cast< char* >(_simpleCommands[i]->_arguments[j]->c_str());
-				}
-				cargument[j] = NULL;
-
-				//execute command
-				execvp(_simpleCommands[i]->_arguments[0]->c_str(), cargument);
+				} else {
 			
-				printf("command not found: %s\n", _simpleCommands[i]->_arguments[0]->c_str());
+					//convert to char** from vector
+					char** cargument = new char*[_simpleCommands[i]->_arguments.size()];
+					unsigned int j;
+					for (j = 0; j < _simpleCommands[i]->_arguments.size(); j++) {
+						cargument[j] = const_cast< char* >(_simpleCommands[i]->_arguments[j]->c_str());
+					}
+					cargument[j] = NULL;
 
-				_exit(1);
-			
+					//execute command
+					execvp(_simpleCommands[i]->_arguments[0]->c_str(), cargument);
+				
+					printf("command not found: %s\n", _simpleCommands[i]->_arguments[0]->c_str());
+
+					_exit(1);
+				}
 				
 			} else if (ret < 0) {
 				perror("fork");
