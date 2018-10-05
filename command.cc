fc72aa4f50b24e08cf6f776fca982c41c0c15bf2
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-05 13:46:29 -0400

    Commit

--- command.cc
+++ command.cc
@@ -380,8 +380,8 @@ void Command::execute() {
 			
 				//source	
 				if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "source")) {
-					/*FILE *fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
-					char cmdline [4096];
+					FILE *fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
+					/*char cmdline [4096];
 					
 					//iterates over each line in the file
 					while (fgets(cmdline, 4095, fp) != NULL) {
@@ -444,11 +444,11 @@ void Command::execute() {
 
 										
 					}
-					
+					*/
 					//closes file descriptor	
 					fclose(fp);
 					
-					*/
+					
 					exit(0);
 			
 				}
