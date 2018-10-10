dc5a8a3797d866b9ab1ec5771dfbb7db670285f7
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-10 14:48:01 -0400

    Commit

--- command.cc
+++ command.cc
@@ -31,7 +31,7 @@
 
 #include "y.tab.hh"
 
-extern MY_YY_BUFFER_STATE
+extern YY_BUFFER_STATE MY_YY_BUFFER_STATE
 extern void my_yy_create_buffer(FILE *file, int size);
 extern void my_yypush_buffer_state(MY_YY_BUFFER_STATE new_buffer);
 extern void my_yyrestart(FILE *input_file);
