fa673055a80b385a858dea9a3bfae6e02673b93d
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-10 14:46:59 -0400

    Commit

--- command.cc
+++ command.cc
@@ -391,6 +391,7 @@ void Command::execute() {
 					FILE *fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
 
 					my_yy_create_buffer(fp, 100);
+					my_yyrestart(fp);
 					yyparse();
 
 
