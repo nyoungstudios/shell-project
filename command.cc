d33cd9460df7448bc0937e60199a5ca300a69943
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-11 00:28:50 -0400

    Commit

--- command.cc
+++ command.cc
@@ -369,7 +369,24 @@ void Command::execute() {
 		
 
 
-			 		
+			//source	
+			if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "source")) {
+				FILE *fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
+
+				my_yy_create_buffer(fp, 100);
+				my_yyrestart(fp);
+				yyparse();
+				my_yy_create_buffer(NULL, 100);
+				my_yyrestart(NULL);
+
+				fclose(fp);
+
+			
+				
+				exit(0);
+		
+			}
+				
 			
 			//fork
 			ret = fork();
@@ -385,7 +402,7 @@ void Command::execute() {
 					exit(0);
 				
 				}
-			
+				/*
 				//source	
 				if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "source")) {
 					FILE *fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
@@ -398,7 +415,7 @@ void Command::execute() {
 
 					fclose(fp);
 
-					/*
+					
 					char cmdline [100];
 					
 					//iterates over each line in the file
@@ -471,14 +488,14 @@ void Command::execute() {
 						
 										
 					}
-					*/	
+					
 					//closes file descriptor	
 					//fclose(fp);
 					
 					
 					exit(0);
 			
-				}
+				}*/
 
 				//convert to char** from vector
 				char** cargument = new char*[_simpleCommands[i]->_arguments.size()];
