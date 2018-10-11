d598a6ded38b063351fb8f423fb3a375b4bf35fd
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-11 00:45:49 -0400

    Commit

--- command.cc
+++ command.cc
@@ -390,15 +390,15 @@ void Command::execute() {
 				if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "source")) {
 					FILE *fp = fopen(_simpleCommands[i]->_arguments[1]->c_str(), "r");
 
-					my_yy_create_buffer(fp, 100);
-					my_yyrestart(fp);
-					yyparse();
+					//my_yy_create_buffer(fp, 100);
+					//my_yyrestart(fp);
+					//yyparse();
 					//my_yy_create_buffer(NULL, 100);
 					//my_yyrestart(NULL);
 
-					fclose(fp);
+					//fclose(fp);
 
-					/*
+					
 					char cmdline [100];
 					
 					//iterates over each line in the file
@@ -459,19 +459,19 @@ void Command::execute() {
 						//sets terminating character
 						buffer[--k] = '\0';
 			
-						//if output, prints it
-						//if (strlen(buffer) > 0) {
-						//	printf("%s\n", buffer);
-						//}
-
-						for (k = strlen(buffer); k >= 0; k--) {
-							myunputc(buffer[k]);
+						if output, prints it
+						if (strlen(buffer) > 0) {
+							printf("%s\n", buffer);
 						}
+
+						//for (k = strlen(buffer); k >= 0; k--) {
+						//	myunputc(buffer[k]);
+						//}
 
 						
 										
 					}
-					*/	
+					
 					//closes file descriptor	
 					//fclose(fp);
 					
