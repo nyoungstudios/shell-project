66ec12fab208bc440ccb3d3b99bdae086803c10b
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-17 21:23:41 -0400

    Commit

--- command.cc
+++ command.cc
@@ -443,7 +443,9 @@ void Command::execute() {
 
 		//checks if not background. If true, waits for command to finish
 		if (!_background) {
-			waitpid(ret, NULL, 0);
+			int *status;
+			waitpid(ret, &status, 0);
+			printf("%d\n", status);
 		} else {
 			//sets background PID variable so can reference in shell.l file
 			backgroundPID = ret;
