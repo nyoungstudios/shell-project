2adba726342980657bd3ed19ad4526d87447d0d0
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-17 22:46:38 -0400

    Commit

--- shell.cc
+++ shell.cc
@@ -37,7 +37,7 @@ void Shell::prompt() {
 }
 
 void controlC(int sig) {
-	printf("\n");
+	printf("\n\r                                      ");
 	Shell::_currentCommand.clear();
 	Shell::prompt();
 	
