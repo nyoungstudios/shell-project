2b9be7d8aa151207eba1d1062d011dcade358f60
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-22 16:11:05 -0400

    Commit

--- command.cc
+++ command.cc
@@ -251,6 +251,7 @@ void Command::execute() {
 			//if error, prints error message
 			if (error < 0) {
 				fprintf(stderr, "cd: can't cd to %s\n", _simpleCommands[0]->_arguments[1]->c_str());
+				lastExitCode = 1;
 			} else {
 				lastExitCode = 0;
 			}
