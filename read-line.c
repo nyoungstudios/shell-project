876f0c9274f3f56016a7771e6967d250413e97f3
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-23 00:37:17 -0400

    Commit

--- read-line.c
+++ read-line.c
@@ -202,7 +202,7 @@ char * read_line() {
 
 				// Copy line from history
 			
-				//printf("--------%d--------%d--------\n", history_length, history_index);
+				printf("--------%d--------%d--------\n", history_length, history_index);
 				if (history_length > 1 && history_index <= history_length - 1) {
 					strcpy(line_buffer, history[history_index]);
 					line_length = strlen(line_buffer);
