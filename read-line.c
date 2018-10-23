641bc05f1dd779c3438e43035c590ea3efb2318f
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-23 00:35:15 -0400

    Commit

--- read-line.c
+++ read-line.c
@@ -208,7 +208,7 @@ char * read_line() {
 					line_length = strlen(line_buffer);
 					
 					//history_index=(history_index+1)%history_length;
-				} else if (history_index == history_length) {
+				} else if (history_index == history_length - 1) {
 					// Erase old line
 					// Print backspaces
 					int i = 0;
