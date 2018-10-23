d3ffc9863890630f4f141bc3f87af0e94e7cf4a8
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-23 00:03:09 -0400

    Commit

--- read-line.c
+++ read-line.c
@@ -166,7 +166,7 @@ char * read_line() {
 				// Copy line from history
 				strcpy(line_buffer, history[history_index]);
 				line_length = strlen(line_buffer);
-				history_index=(history_index+1)%history_length;
+				history_index=(history_index-1)%history_length;
 
 				// echo line
 				write(1, line_buffer, line_length);
