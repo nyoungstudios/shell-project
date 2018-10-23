072d0397b41706b7f141b6fcaa64fc19de6d3332
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-23 00:52:15 -0400

    Commit

--- read-line.c
+++ read-line.c
@@ -204,12 +204,12 @@ char * read_line() {
 			
 				printf("--------%d--------%d--------\n", history_length, history_index);
 				if (history_length > 1 && history_index < history_length - 1) {
-					history_index = history_index + 2;
+					//history_index = history_index + 2;
 					strcpy(line_buffer, history[history_index]);
 					line_length = strlen(line_buffer);
 					history_index++;	
 					//history_index=(history_index+1)%history_length;
-				} else if (history_index == history_length) {
+				} else if (history_index == history_length - 1) {
 					// Erase old line
 					// Print backspaces
 					int i = 0;
@@ -232,7 +232,7 @@ char * read_line() {
 				}
 				// echo line
 				write(1, line_buffer, line_length);
-				history_index--;
+				//history_index--;
 
 			}
       
