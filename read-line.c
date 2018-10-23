0a811fc8df9bf8c6b6de3cd3915e7e47d4300743
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-22 22:51:29 -0400

    Commit

--- read-line.c
+++ read-line.c
@@ -177,8 +177,7 @@ char * read_line() {
   line_length++;
   line_buffer[line_length]=0;
 
-	printf("this is the line: %d\n", strcmp(line_buffer, ""));
-
+	//checks for empty line
 	if (strcmp(line_buffer, "") != 10) {
 
 		//update history
