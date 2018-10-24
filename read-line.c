105019a55910fa7aa2a87fcaa8edcb69256f4f23
nyoungstudios <nathanielyoung95070@gmail.com>
2018-10-24 14:09:16 -0400

    Commit

--- read-line.c
+++ read-line.c
@@ -261,7 +261,8 @@ char * read_line() {
 					//history_index++;	
 					//history_index=(history_index+1)%history_length;
 				} else if (history_index >= history_length - 1) {
-					
+				
+					printf("%d\n", history_length);
 					// Erase old line
 					// Print backspaces
 					int i = 0;
